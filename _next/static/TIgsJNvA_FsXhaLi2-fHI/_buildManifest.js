13c89f72b0d1dedada37a54e747e02dd318df66c
Eddy02136 <Eddy02136@users.noreply.github.com>
2026-04-29 04:53:54 +0000

    deploy: 7e55aa37eafbacc0ef683da747f959dbafd58ed2

--- _next/static/TIgsJNvA_FsXhaLi2-fHI/_buildManifest.js
+++ _next/static/TIgsJNvA_FsXhaLi2-fHI/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Shimera-web//_next/:path+",
+        "destination": "/Shimera-web/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
